b272dc22d5975a47b7d98a3e72b56e9206da14ef
Amynllie Ann Perez <amynlliep@gmail.com>
2023-12-19 17:37:23 +0800

    Create testv5.java

---
+++ testv5.java
@@ -0,0 +1,13 @@
+"AHVN"/>
+<Entry headword="AVSN"/>
+<Entry headword="AHV Number"/>
+<Entry headword="AVS Number"/>
+<Entry headword="AHV"/>
+<Entry headword="AVS"/>
+<Entry headword="Swiss AHV"/>
+<Entry headword="Swiss AVS"/>
+<Entry headword="Swiss Social Security"/>
+<Entry headword="AHV Identification"/>
+<Entry headword="AVS Identification"/>
+<Entry headword="Swiss Identification"/>
+<Entry headword="AHV ID"/>
